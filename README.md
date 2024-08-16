9055d147f6ea9dce0806b69e62d6297b1f980633
Anh Tuấn <anhtuan.bkfet.k55@gmail.com>
2024-08-16 10:44:11 +0700

    Add basic authen

--- README.md
+++ README.md
@@ -13,11 +13,11 @@ The main feature are:
 - **Platform Names**: Each platform supported must be created on the web interface before a binary is uploaded. If an existing _platform name_ is not found in an uploaded binary it is rejected. Multiple devices can share the same _platform name_ and thus receive the same binary. Individual devices are controlled through _whitelists_ described below.
 - **Semantic Versioning**: Updates are only done if a newer version of a binary is available compared to the version the device is already running. Semantic versioning is assumed e.g. v1.0.2. The uploaded binary must contain a version number starting with _v_ and three version number components i.e MAJOR, MINOR, PATCH - _v1.0.2_. An uploaded binary is rejected if such a version number is not found in the binary or if the version number is not increased compared to the one already known. 
 - **Whitelists**: Download control is enforced by MAC Address whitelists. On the web interface WiFi MAC Addresses can be added and removed to created platforms. Only whitelisted devices will be allowed to update.
-- **Binary Upload**: Uploading binaries is simple and administration is kept to a minimum by automatic detection of _platform name_ and _version number_.
+- **Binary Upload**: Uploading binaries is simple and administration is kept to a minimum by automatic detection of _platform name_ and _version number_. This detection helps prevent mistakes where the OTA update routine is not called or you forgot to enter a valid platform name.
 
 ## How Do I Use It?
 
-The server is _intended_ to run on internal network where it cannot be accessed from the internet. As such it does not offer any security mechanisms.
+The server is _intended_ to run on internal network where it cannot be accessed from the internet. As such it does only offer very basic security. In the users.yml, you can create additional users that are allowed to access the server. If no users.yml file is found, the server runs without authentication.
 
 ### Start Server From Code
 
@@ -27,28 +27,39 @@ To run the server directly from code start it with the following command:
 python3 server.py
 ```
 
+### Build docker file yourself
+From the root-directory of this app, run: `docker build -t esp-update-server:latest . ` to re-build the docker-image from source
+To directly run this app, run
+```
+docker run -d --restart unless-stopped --name esp-update-server -v $PWD/bin:/esp-update-server/bin -p 5000:5000 esp-update-server:latest
+```
+
 ### Start Server From Docker?
 
 Ready-made Docker images are available on [Docker Hub](https://hub.docker.com/r/kstobbe/esp-update-server/) which support running on Linux on both AMD64 and ARM32V6 architectures - i.e. desktops, laptops, and Raspberry Pis.
 
-To run the server in a Docker container create a directory for storing binaries. Go inside this directory and execute the following command:
+To run the server in a Docker container create a directory `bin` for storing binaries. Then run following command from the directory where you have the `server.py`
 
 ```
-docker run -d -v $PWD:/esp-update-server/bin -p 5000:5000 kstobbe/esp-update-server:latest
+docker run -d --restart unless-stopped --name esp-update-server -v $PWD/bin:/esp-update-server/bin -p 5000:5000 kstobbe/esp-update-server:latest
 ```
 
 Using the `-v` option ensures files are stored outside the Docker container and are thus persisted even if the container is terminated.
 
 ### Access Server For Management
 
-In a web browser, when the server is running, enter the IP address of the machine running the server and port 5000, e.g. `http://192.168.0.10:5000`. Now platforms can be created and deleted. Whitelists can be managed and binaries uploaded.
+In a web browser, when the server is running, enter the IP address of the machine running the server and port 5000, e.g. `http://192.168.0.10:5000`. Now platforms can be created and deleted. Whitelists can be managed and binaries uploaded.  
+**Status overview**  
+![alt text](img/status.png "Status overview")  
+**Whitelisting devices, and assigning them to a platform**  
+![alt text](img/whitelist.png "Whitelist page")  
 
 ### Access Server For Update
 
 Devices requesting download of a binary file for upgrade must access path `update` and include _device name_ and current _version number_ in a query like below - substitute the IP address with your own.
 
 ```
-http://192.168.0.10:5000/update?ver=v1.0.2&dev=chase
+http://192.168.0.10:5000/update?dev=chase&ver=v1.0.2
 ```
 
 The server will respond with _HTTP Error Code_:
@@ -65,17 +76,15 @@ Below if an implementation for _ESP32_ that works with the server. Remember to c
 #include <HTTPUpdate.h>
 #include <WiFi.h>
 
-#define VERSION "v1.0.2"
-#define HOST "Chase"
+#define VERSION "v1.0.2
+#define DEVICE_PLATFORM "Chase"
 
-const char* urlBase = "http://192.168.0.10:5000/update";
+const char* ota_update_server = "http://192.168.0.10:5000/";
 
 /***************************************************/
 void checkForUpdates(void)
 {
-  String checkUrl = String( urlBase);
-  checkUrl.concat( "?ver=" + String(VERSION) );
-  checkUrl.concat( "&dev=" + String(HOST) );
+  String checkUrl = String( ota_update_server) + String("update?dev=" DEVICE_PLATFORM "&ver=" VERSION );
 
   Serial.println("INFO: Checking for updates at URL: " + String( checkUrl ) );
 
@@ -106,17 +115,15 @@ For _ESP8266_ the implementation is very similar with a few changes. Remember to
 #include <ESP8266httpUpdate.h>
 #include <ESP8266WiFi.h>
 
-#define VERSION "v1.0.2"
-#define HOST "Chase"
+#define VERSION "v1.0.2
+#define DEVICE_PLATFORM "Chase"
 
-const char* urlBase = "http://192.168.0.10:5000/update";
+const char* ota_update_server = "http://192.168.0.10:5000/";
 
 /***************************************************/
 void checkForUpdates(void)
 {
-  String checkUrl = String( urlBase);
-  checkUrl.concat( "?ver=" + String(VERSION) );
-  checkUrl.concat( "&dev=" + String(HOST) );
+  String checkUrl = String( ota_update_server) + String("update?dev=" DEVICE_PLATFORM "&ver=" VERSION );
 
   Serial.println("INFO: Checking for updates at URL: " + String( checkUrl ) );
   
--- server.py
+++ server.py
@@ -1,26 +1,48 @@
-from datetime import datetime
-from flask import Flask, request, render_template, flash, redirect, url_for, send_from_directory
-from packaging import version
+import os
 import re
 import time
-import os
+from datetime import datetime
+from collections import OrderedDict
+
 import yaml
+from flask import (Flask, flash, redirect, render_template, request,
+                   send_from_directory, url_for)
+from flask_moment import Moment
+from packaging import version
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 __author__ = 'Kristian Stobbe'
 __copyright__ = 'Copyright 2019, K. Stobbe'
 __credits__ = ['Kristian Stobbe']
 __license__ = 'MIT'
-__version__ = '1.1.0'
+__version__ = '2.1.0'
 __maintainer__ = 'Kristian Stobbe'
 __email__ = 'mail@kstobbe.dk'
 __status__ = 'Production'
 
 ALLOWED_EXTENSIONS = set(['bin'])
 app = Flask(__name__)
+moment = Moment(app)
 app.config['UPLOAD_FOLDER'] = './bin'
 app.config['SECRET_KEY'] = 'Kri57i4n570bb33r3nF1ink3rFyr'
 PLATFORMS_YAML = app.config['UPLOAD_FOLDER'] + '/platforms.yml'
+MACS_YAML = app.config['UPLOAD_FOLDER'] + '/macs.yml'
+USERS_YAML = app.config['UPLOAD_FOLDER'] + '/users.yml'
 
+
+auth = HTTPBasicAuth()
+
+users = {}
+
+@auth.verify_password
+def verify_password(username, password):
+    if not users:
+        return True
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
+    return False
 
 def log_event(msg):
     st = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
@@ -49,6 +71,24 @@ def load_yaml():
                     value['whitelist'][i] = str(value['whitelist'][i])
     return platforms
 
+def load_users():
+    users = None
+    try:
+        with open(USERS_YAML, 'r') as stream:
+            try:
+                users = yaml.load(stream, Loader=yaml.FullLoader)
+            except yaml.YAMLError as err:
+                print(err)
+    except:
+        print('Warning: Users file not found, no authentication.')
+    if users:
+        for user in users: # generate hash from the plaintext password
+            users[user] = generate_password_hash(users[user])
+        print(users)
+    if not users:
+        users = dict()
+    return users
+
 
 def save_yaml(platforms):
     try:
@@ -59,6 +99,46 @@ def save_yaml(platforms):
         flash('Error: Data not saved.')
     return False
 
+
+def load_known_mac_yaml():
+    macs = None
+    try:
+        with open(MACS_YAML, 'r') as stream:
+            try:
+                macs = yaml.load(stream, Loader=yaml.FullLoader)
+            except yaml.YAMLError as err:
+                flash(err)
+    except:
+        flash('Error: File not found.')
+    if macs:
+        for known_mac in macs.values():
+            if known_mac['first_seen']:
+                known_mac['first_seen'] = str(known_mac['first_seen'])
+                known_mac['first_seen_dt'] = datetime.strptime(known_mac['first_seen'], '%Y-%m-%d %H:%M:%S')
+            if known_mac['last_seen']:
+                known_mac['last_seen'] = str(known_mac['last_seen'])
+                known_mac['last_seen_dt'] = datetime.strptime(known_mac['last_seen'], '%Y-%m-%d %H:%M:%S')
+    if not macs:
+        macs = dict()
+    return macs
+
+
+def save_known_mac_yaml(macs):
+    try:
+        with open(MACS_YAML, 'w') as outfile:
+            yaml.dump(macs, outfile, default_flow_style=False)
+            return True
+    except:
+        flash('Error: Known MAC data not saved.')
+    return False
+
+def detect_known_macs(known_macs, platforms):
+    for key, known_mac in known_macs.items():
+        for platform, platform_values in platforms.items():
+            if(platform_values['whitelist'] and key in platform_values['whitelist']):
+                # This mac is whitelisted, store the platform-name, so we can use that in the future
+                known_mac['platform'] = platform 
+    return known_macs
 
 @app.context_processor
 def utility_processor():
@@ -71,6 +151,7 @@ def utility_processor():
 def update():
     __error = 400
     platforms = load_yaml()
+    known_macs = load_known_mac_yaml()
     __dev = request.args.get('dev', default=None)
     if 'X_ESP8266_STA_MAC' in request.headers:
         __mac = request.headers['X_ESP8266_STA_MAC']
@@ -85,11 +166,23 @@ def update():
         log_event("WARN: Update called without known headers.")
     __ver = request.args.get('ver', default=None)
     if __dev and __mac and __ver:
+        # If we know this device already
+        if __mac in known_macs.keys():
+            known_macs[__mac]['last_seen'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            known_macs[__mac] = {'first_seen': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                           'last_seen': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                           'IP': None,
+                           'type': None}
+        save_known_mac_yaml(known_macs)
         log_event("INFO: Dev: " + __dev + "Ver: " + __ver)
         __dev = __dev.lower()
         if platforms:
             if __dev in platforms.keys():
                 if __mac in platforms[__dev]['whitelist']:
+                    if not platforms[__dev]['version']:
+                        log_event("ERROR: No update available.")
+                        return 'No update available.', 400
                     if version.parse(__ver) < version.parse(platforms[__dev]['version']):
                         if os.path.isfile(app.config['UPLOAD_FOLDER'] + '/' + platforms[__dev]['file']):
                             platforms[__dev]['downloads'] += 1
@@ -112,8 +205,8 @@ def update():
     log_event("ERROR: Invalid parameters.")
     return 'Error: Invalid parameters.', 400
 
-
 @app.route('/upload', methods=['GET', 'POST'])
+@auth.login_required
 def upload():
     platforms = load_yaml()
     if platforms and request.method == 'POST':
@@ -127,39 +220,40 @@ def upload():
         if file and allowed_ext(file.filename):
             data = file.read()
             for __dev in platforms.keys():
-                if re.search(__dev.encode('UTF-8'), data, re.IGNORECASE):
-                    m = re.search(b'v\d+\.\d+\.\d+', data)
-                    if m:
-                        __ver = m.group()[1:].decode('utf-8')
-                        if (platforms[__dev]['version'] is None) or (platforms[__dev]['version'] and version.parse(platforms[__dev]['version']) < version.parse(__ver)):
-                            old_file = platforms[__dev]['file']
-                            filename = __dev + '_' + __ver.replace('.', '_') + '.bin'
-                            platforms[__dev]['version'] = __ver
-                            platforms[__dev]['downloads'] = 0
-                            platforms[__dev]['file'] = filename
-                            platforms[__dev]['uploaded'] = datetime.now().strftime('%Y-%m-%d')
-                            file.seek(0)
-                            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                            file.close()
-                            if save_yaml(platforms):
-                                # Only delete old file after YAML file is updated.
-                                if old_file:
-                                    try:
-                                        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], old_file))
-                                    except:
-                                        flash('Error: Removing old file failed.')
-                                flash('Success: File uploaded.')
-                            else:
-                                flash('Error: Could not save file.')
-                            return redirect(url_for('index'))
+                m = re.search(b"update\?dev=" + __dev.encode('UTF-8') + b"&ver=(v\d+\.\d+\.\d+)\x00", data, re.IGNORECASE)
+                if m:
+                    __ver = m.groups()[0][1:].decode('utf-8')
+                    if (platforms[__dev]['version'] is None) or (platforms[__dev]['version'] and version.parse(platforms[__dev]['version']) < version.parse(__ver)):
+                        old_file = platforms[__dev]['file']
+                        filename = __dev + '_' + __ver.replace('.', '_') + '.bin'
+                        platforms[__dev]['version'] = __ver
+                        platforms[__dev]['downloads'] = 0
+                        platforms[__dev]['file'] = filename
+                        platforms[__dev]['uploaded'] = datetime.now().strftime('%Y-%m-%d')
+                        file.seek(0)
+                        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                        file.close()
+                        if save_yaml(platforms):
+                            # Only delete old file after YAML file is updated.
+                            if old_file:
+                                try:
+                                    os.remove(os.path.join(app.config['UPLOAD_FOLDER'], old_file))
+                                except:
+                                    flash('Error: Removing old file failed.')
+                            flash('Success: File uploaded for platform {} with version {}.'.format(__dev, __ver))
                         else:
-                            flash('Error: Version must increase. File not uploaded.')
-                            return redirect(request.url)
+                            flash('Error: Could not save file.')
+                        return redirect(url_for('index'))
                     else:
-                        flash('Error: No version found in file. File not uploaded.')
+                        flash('Error: Version must increase. File not uploaded.')
                         return redirect(request.url)
-            flash('Error: No known platform name found in file. File not uploaded.')
-            return redirect(request.url)
+            m = re.search(b"update\?dev=" + __dev.encode('UTF-8')+ b"&ver=$", data, re.IGNORECASE)
+            if m: # a platform was found, meaning no version was found
+                flash('Error: No version found in file. File not uploaded.')
+                return redirect(request.url)
+            else:
+                flash('Error: No known platform name found in file. File not uploaded.')
+                return redirect(request.url)
         else:
             flash('Error: File type not allowed.')
             return redirect(request.url)
@@ -170,6 +264,7 @@ def upload():
 
 
 @app.route('/create', methods=['GET', 'POST'])
+@auth.login_required
 def create():
     if request.method == 'POST':
         if not request.form['name']:
@@ -193,6 +288,7 @@ def create():
 
 
 @app.route('/delete', methods=['GET', 'POST'])
+@auth.login_required
 def delete():
     if request.method == 'POST':
         if not request.form['name']:
@@ -222,8 +318,10 @@ def delete():
 
 
 @app.route('/whitelist', methods=['GET', 'POST'])
+@auth.login_required
 def whitelist():
     platforms = load_yaml()
+    known_macs = load_known_mac_yaml()
     if platforms and request.method == 'POST':
         if 'Add' in request.form['action']:
             # Ensure valid data.
@@ -257,18 +355,19 @@ def whitelist():
                 flash('Error: Could not save file.')
         else:
             flash('Error: Unknown action.')
-
     if platforms:
-        return render_template('whitelist.html', platforms=platforms)
+        known_macs_with_platform =  detect_known_macs(known_macs, platforms)
+        return render_template('whitelist.html', platforms=platforms, known_macs = known_macs_with_platform)
     else:
         return render_template('status.html', platforms=platforms)
 
-
 @app.route('/')
+@auth.login_required
 def index():
     platforms = load_yaml()
     return render_template('status.html', platforms=platforms)
 
 
 if __name__ == '__main__':
+    users = load_users()
     app.run(host='0.0.0.0', port=int('5000'), debug=False)
